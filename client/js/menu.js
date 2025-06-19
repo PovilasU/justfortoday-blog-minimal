cf19428ff2167fc5f25ede487c374f3f73f43da1
PovilasU <povilas.urbonas85@gmail.com>
2025-06-19 22:04:50 +0300

    added styling

---
+++ client/js/menu.js
@@ -0,0 +1,19 @@
+// menu.js
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.createElement("nav");
+  nav.innerHTML = `
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="about.html">About</a></li>
+      <li><a href="about.html">todo</a></li>
+      <li><a href="about.html">Media</a></li>
+      <li><a href="about.html">Meditation</a></li>
+      <li><a href="about.html">Stuff</a></li>
+      <li><a href="about.html">Day Reflection</a></li>
+    </ul>
+  `;
+  // nav.style.padding = "1em";
+  // nav.style.background = "#f0f0f0";
+  // nav.style.marginBottom = "1em";
+  document.body.prepend(nav);
+});
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ const path = require("path");
 const app = express();
 
 app.use(serveCompressed);
-app.use(express.static(path.resolve(__dirname, "../client/dist")));
+//app.use(express.static(path.resolve(__dirname, "../client/dist")));
+app.use(express.static(path.resolve(__dirname, "../client")));
 
 app.listen(3000, () => console.log("Server running on http://localhost:3000"));
