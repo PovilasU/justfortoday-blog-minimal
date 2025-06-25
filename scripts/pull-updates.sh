63f1c23c82e3a19260c2c483b7123f7a35691818
PovilasU <povilas.urbonas85@gmail.com>
2025-06-25 11:41:29 +0300

    making quet git pull

--- scripts/pull-updates.sh
+++ scripts/pull-updates.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# Navigate to the project folder
 cd /home/povilas/justfortoday-blog-minimal || exit
-
-# Pull latest changes and log output
-/usr/bin/git pull >> /home/povilas/justfortoday-blog-minimal/git-pull.log 2>&1
+/usr/bin/git pull --quiet
